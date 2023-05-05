c585f03b463d919c687fd79e6455c3dc016a5252
Laura Hope <ssteamlaura@gmail.com>
2023-05-05 13:18:04 -0700

    change syntax to include auto-implemented properties for Bread and Pastry classes.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -5,15 +5,19 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    public static int GetBreadPrice(int input)
+    public int BreadOrder { get; set; }
+    public Bread(int breadQuantity)
     {
+      BreadOrder = breadQuantity;
+    }
+    public int GetBreadPrice(int input)
+    {
+      int totalPrice = 0;
       int price = 5;
-      int discount = 0;
-      if (input >= 3)
-      {
-        discount = input / 3;
-      }
-      return (input * price) - (discount * price);
+      int discountTotal = input/3;
+      int discount = discountTotal * price;
+      totalPrice = (input * price) - discount;
+      return totalPrice;
     }
   }
-}
+} 
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -5,30 +5,19 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-    // public int PastryOrder { get; set; }
-    // public Pastry(int pastryQuantity)
-    // {
-    //   PastryOrder = pastryQuantity;
-    // }
-    // public int GetPastryPrice(int pastryQuantity)
-    // {
-    //   int price = 2;
-    //   int discount = 0;
-    //   if (pastryQuantity >= 4)
-    //   {
-    //     discount = pastryQuantity / 4;
-    //   }
-    //   return (pastryQuantity * price) - (discount * price);
-    // }
-    public static int GetPastryPrice(int input)
+    public int PastryOrder { get; set; }
+    public Pastry(int pastryQuantity)
     {
+      PastryOrder = pastryQuantity;
+    }
+    public int GetPastryPrice(int input)
+    {
+      int totalPrice = 0;
       int price = 2;
-      int discount = 0;
-      if (input >= 4)
-      {
-        discount = input / 4;
-      }
-      return (input * price) - (discount * price);
+      int discountTotal = input/4;
+      int discount = discountTotal * price;
+      totalPrice = (input * price) - discount;
+      return totalPrice;
     }
   }
 }
