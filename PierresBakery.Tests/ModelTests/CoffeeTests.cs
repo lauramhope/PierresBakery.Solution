d72d655431d52ea47c7aecee0c93515b9e9553c6
Laura Hope <ssteamlaura@gmail.com>
2023-05-05 14:54:27 -0700

    Add passing test for setting coffee price with auto-implemented properties

--- PierresBakery.Tests/ModelTests/CoffeeTests.cs
+++ PierresBakery.Tests/ModelTests/CoffeeTests.cs
@@ -7,12 +7,17 @@ namespace PierresBakery.Tests
   [TestClass]
   public class CoffeeTests
   {
-    Coffee newCoffee = new Coffee();
+    Coffee newCoffee = new Coffee(1);
     [TestMethod]
     public void CoffeeConstructor_CreatesInstanceOfCoffee_Coffee()
     {
       Assert.AreEqual(typeof(Coffee), newCoffee.GetType());
     }
 
+    [TestMethod]
+    public void GetCoffeePrice_ReturnsCoffeePrice_Int()
+    {
+      Assert.AreEqual(3, newCoffee.GetCoffeePrice(1));
+    }
   }
 }
