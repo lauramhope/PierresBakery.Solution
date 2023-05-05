a9ef6c407df40260a42ecee2162ca03ef2714f5d
Laura Hope <ssteamlaura@gmail.com>
2023-05-05 08:13:54 -0700

    Change project file names to PierresBakery.

---
+++ PierresBakery/Models/ClassName.cs
@@ -0,0 +1,10 @@
+using System; 
+using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+  public class ClassName
+  {
+    // properties, constructors, methods, etc. go here
+  }
+}
