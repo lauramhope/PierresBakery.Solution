0122b635befa7064f686c905c8843eb24e09f370
Laura Hope <ssteamlaura@gmail.com>
2023-05-05 10:55:44 -0700

    Add working error handling via try-catch block for non-int input

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,27 +7,38 @@ namespace PierresBakery
   {
     static void Main() 
     {
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("Bread is $5 per loaf, or buy 2 get 1 free!");
-      Console.WriteLine("~* Every 3rd loaf is free *~");
-      Console.WriteLine("Pastries are $2 each, or buy 3 get 1 free!");
-      Console.WriteLine("~* Every 4th pastry is free *~");
-      Console.ResetColor();
+      try 
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Welcome to Pierre's Bakery!");
+        Console.WriteLine("Bread is $5 per loaf, or buy 2 get 1 free!");
+        Console.WriteLine("~* Every 3rd loaf is free *~");
+        Console.WriteLine("Pastries are $2 each, or buy 3 get 1 free!");
+        Console.WriteLine("~* Every 4th pastry is free *~");
+        Console.ResetColor();
 
-      Bread newBread = new Bread();
-      Pastry newPastry = new Pastry();
+        Bread newBread = new Bread();
+        Pastry newPastry = new Pastry();
 
-      Console.WriteLine("How many loaves of bread would you like?");
-      int breadInput = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many loaves of bread would you like?");
+        int breadInput = int.Parse(Console.ReadLine());
 
-      Console.WriteLine("How many pastries would you like?");
-      int pastryInput = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many pastries would you like?");
+        int pastryInput = int.Parse(Console.ReadLine());
 
-      int breadPrice = Bread.GetBreadPrice(breadInput);
-      int pastryPrice = Pastry.GetPastryPrice(pastryInput);
-      int totalPrice = breadPrice + pastryPrice;
-      Console.WriteLine("Your total is $" + totalPrice + ".00");
+        int breadPrice = Bread.GetBreadPrice(breadInput);
+        int pastryPrice = Pastry.GetPastryPrice(pastryInput);
+        int totalPrice = breadPrice + pastryPrice;
+        Console.WriteLine("Your total is $" + totalPrice + ".00");
+      }
+      catch (Exception ex)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Message = {0}", ex.Message);
+        Console.WriteLine("Please enter a valid number.");
+        Console.ResetColor();
+        Main();
+      }
     }
   }
 }
