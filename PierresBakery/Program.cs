a9b05c1ab71e49123ef314555695a624824e5807
Laura Hope <ssteamlaura@gmail.com>
2023-05-05 13:32:23 -0700

    add error handling via try-catch block for non-int input

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -9,24 +9,35 @@ namespace PierresBakery
   {
     static void Main() 
     {
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("Bread is $5 per loaf, or buy 2 get 1 free!");
-      Console.WriteLine("~* Every 3rd loaf is free *~");
-      Console.WriteLine("Pastries are $2 each, or buy 3 get 1 free!");
-      Console.WriteLine("~* Every 4th pastry is free *~");
-      Console.ResetColor();
-      
-      Console.WriteLine("How many loaves of bread would you like?");
-      int breadQuantity = int.Parse(Console.ReadLine());
-      Console.WriteLine("How many pastries would you like?");
-      int pastryQuantity = int.Parse(Console.ReadLine());
-      Bread newBread = new Bread(breadQuantity);
-      Pastry newPastry = new Pastry(pastryQuantity);
-      int breadPrice = newBread.GetBreadPrice(breadQuantity);
-      int pastryPrice = newPastry.GetPastryPrice(pastryQuantity);
-      int totalPrice = breadPrice + pastryPrice;
-      Console.WriteLine("Your total is $" + totalPrice + ". Thank you for shopping at Pierre's Bakery!");
+      try 
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Welcome to Pierre's Bakery!");
+        Console.WriteLine("Bread is $5 per loaf, or buy 2 get 1 free!");
+        Console.WriteLine("~* Every 3rd loaf is free *~");
+        Console.WriteLine("Pastries are $2 each, or buy 3 get 1 free!");
+        Console.WriteLine("~* Every 4th pastry is free *~");
+        Console.ResetColor();
+
+        Console.WriteLine("How many loaves of bread would you like?");
+        int breadQuantity = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many pastries would you like?");
+        int pastryQuantity = int.Parse(Console.ReadLine());
+        Bread newBread = new Bread(breadQuantity);
+        Pastry newPastry = new Pastry(pastryQuantity);
+        int breadPrice = newBread.GetBreadPrice(breadQuantity);
+        int pastryPrice = newPastry.GetPastryPrice(pastryQuantity);
+        int totalPrice = breadPrice + pastryPrice;
+        Console.WriteLine("Your total is $" + totalPrice + ". Thank you for shopping at Pierre's Bakery!");
+      }
+      catch (Exception ex)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Message = {0}", ex.Message);
+        Console.WriteLine("Please enter a valid number.");
+        Console.ResetColor();
+        Main();
+      }
     }
   }
 }
