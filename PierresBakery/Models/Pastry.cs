51776aee057ec20f4d2e06afcf7c3d62fbda7af2
Laura Hope <ssteamlaura@gmail.com>
2023-05-05 12:00:02 -0700

    WIP: Lunch break

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -5,6 +5,21 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
+    // public int PastryOrder { get; set; }
+    // public Pastry(int pastryQuantity)
+    // {
+    //   PastryOrder = pastryQuantity;
+    // }
+    // public int GetPastryPrice(int pastryQuantity)
+    // {
+    //   int price = 2;
+    //   int discount = 0;
+    //   if (pastryQuantity >= 4)
+    //   {
+    //     discount = pastryQuantity / 4;
+    //   }
+    //   return (pastryQuantity * price) - (discount * price);
+    // }
     public static int GetPastryPrice(int input)
     {
       int price = 2;
